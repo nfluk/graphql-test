3845459c5ba0e5666494a33842ba9d19e2d51a1e
Nick Lukic <nick@iccmediasport.com>
2023-11-14 11:35:58 +0100

    added related data functionality

--- _db.js
+++ _db.js
@@ -14,6 +14,12 @@ let reviews = [
     {id: "5", rating: '8', content: 'Great game', author_id: "2", game_id: "5"},
     {id: "6", rating: '7', content: 'Great game', author_id: "1", game_id: "3"},
     {id: "7", rating: '10', content: 'Great game', author_id: "2", game_id: "1"},
+    {id: "8", rating: '7', content: 'Great game', author_id: "4", game_id: "4"},
+    {id: "9", rating: '5', content: 'Great game', author_id: "5", game_id: "5"},
+    {id: "10", rating: '9', content: 'Great game', author_id: "3", game_id: "2"},
+    {id: "11", rating: '10', content: 'Great game', author_id: "3", game_id: "5"},
+    {id: "12", rating: '8', content: 'Great game', author_id: "1", game_id: "1"},
+    {id: "13", rating: '10', content: 'Great game', author_id: "2", game_id: "4"},
 ]
 
 let authors = [
