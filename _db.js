808022be0bcafc2251c40c8a19b0c8d86e0a1049
Nick Lukic <nick@iccmediasport.com>
2023-11-14 10:48:16 +0100

    added resolvers and finshed server setup

--- _db.js
+++ _db.js
@@ -1,23 +1,25 @@
 let games = [
-    {id: 1, title: 'Zelda, Tears of the Kingdom', platform: ['Switch']},
-    {id: 2, title: 'Final Fantasy 7 Remake', platform: ['PS5', 'Xbox']},
-    {id: 3, title: 'Elden Ring', platform: ['PS5', 'Xbox', 'PC']},
-    {id: 4, title: 'Mario Kart', platform: ['Switch']},
-    {id: 5, title: 'Pokemon Scarlet', platform: ['PS5', 'Xbox', 'PC']},
+    {id: "1", title: 'Zelda, Tears of the Kingdom', platform: ['Switch']},
+    {id: "2", title: 'Final Fantasy 7 Remake', platform: ['PS5', 'Xbox']},
+    {id: "3", title: 'Elden Ring', platform: ['PS5', 'Xbox', 'PC']},
+    {id: "4", title: 'Mario Kart', platform: ['Switch']},
+    {id: "5", title: 'Pokemon Scarlet', platform: ['PS5', 'Xbox', 'PC']},
 ]
 
 let reviews = [
-    {id: 1, rating: '9', content: 'Great game', author_id: "1", game_id: "1"},
-    {id: 2, rating: '10', content: 'Great game', author_id: "2", game_id: "2"},
-    {id: 3, rating: '7', content: 'Great game', author_id: "3", game_id: "3"},  
-    {id: 4, rating: '5', content: 'Great game', author_id: "3", game_id: "4"},
-    {id: 5, rating: '8', content: 'Great game', author_id: "2", game_id: "5"},
-    {id: 6, rating: '7', content: 'Great game', author_id: "1", game_id: "3"},
-    {id: 7, rating: '10', content: 'Great game', author_id: "2", game_id: "1"},
+    {id: "1", rating: '9', content: 'Great game', author_id: "1", game_id: "1"},
+    {id: "2", rating: '10', content: 'Great game', author_id: "2", game_id: "2"},
+    {id: "3", rating: '7', content: 'Great game', author_id: "3", game_id: "3"},  
+    {id: "4", rating: '5', content: 'Great game', author_id: "3", game_id: "4"},
+    {id: "5", rating: '8', content: 'Great game', author_id: "2", game_id: "5"},
+    {id: "6", rating: '7', content: 'Great game', author_id: "1", game_id: "3"},
+    {id: "7", rating: '10', content: 'Great game', author_id: "2", game_id: "1"},
 ]
 
 let authors = [
-    {id: 1, name: 'mario', verified: true},
-    {id: 2, name: 'yoshi', verified: false},
-    {id: 3, name: 'peach', verified: true}
+    {id: "1", name: 'mario', verified: true},
+    {id: "2", name: 'yoshi', verified: false},
+    {id: "3", name: 'peach', verified: true}
 ]
+
+export default {games, reviews, authors}
